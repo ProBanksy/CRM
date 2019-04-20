e844c8e18ee41ab31527dc477f1056439c8a9734
нαяяү вαηкs <spongebob96@hotmail.co.uk>
2019-04-20 21:47:11 +0100

    Next commit

---
+++ CRM.REST/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using CRM.REST.Models;
+using CRM.REST.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRM.REST.Controllers
+{
+    [Route("[controller]")]
+    [ApiController]
+    public class UsersController : Controller
+    {
+        // GET: <controller>
+        [HttpGet]
+        public JsonResult GetAll()
+        {
+            Console.WriteLine("In GetAll");
+            return Json(UserService.GetInstance().GetAll());
+        }
+
+        // POST: <controller>
+        [HttpPost("Add")]
+        public JsonResult Add(UserModel user)
+        {
+            Console.WriteLine("In Add");
+            return Json(UserService.GetInstance().Add(user));
+        }
+
+        // PUT: <controller>
+        [HttpPut("Update")]
+        public JsonResult Update(UserModel user)
+        {
+            Console.WriteLine("In Update");
+            return Json(UserService.GetInstance().Update(user));
+        }
+
+        // DELETE: <controller>
+        [HttpDelete("Delete/{id}")]
+        public JsonResult Delete(string id)
+        {
+            Console.WriteLine("In Delete");
+            return Json(UserService.GetInstance().Delete(id));
+        }
+    }
+}
---
+++ CRM.REST/Services/UserService.cs
@@ -0,0 +1,67 @@
+﻿using CRM.REST.Models;
+using System.Collections.Generic;
+
+namespace CRM.REST.Services
+{
+    public class UserService
+    {
+        List<UserModel> userList;
+        static UserService userService = null;
+
+        public UserService()
+        {
+            userList = new List<UserModel>();
+        }
+
+        public static UserService GetInstance()
+        {
+            if (userService == null)
+            {
+                userService = new UserService();
+                return userService;
+            }
+            else
+            {
+                return userService;
+            }
+        }
+
+        public List<UserModel> GetAll()
+        {
+            return userList;
+        }
+
+        public string Add(UserModel user)
+        {
+            userList.Add(user);
+            return "Add successful";
+        }
+
+        public string Update(UserModel model)
+        {
+            for (int i = 0; i < userList.Count; i++)
+            {
+                if (userList[i].Id.Equals(model.Id))
+                {
+                    userList[i] = model;
+                    return "Update successful";
+                }
+            }
+            return "Update un-successful; Id not found";
+        }
+
+        public string Delete(string id)
+        {
+            for (int i = 0; i < userList.Count; i++)
+            {
+                UserModel user = userList[i];
+                if (user.Id.Equals(id))
+                {
+                    userList.RemoveAt(i);
+                    return "Delete successful";
+                }
+            }
+            return "Delete un-successful; Id not found";
+        }
+    }
+}
